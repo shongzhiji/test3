d6ce81f0d4f5096f0f098bb3504dbdd975a0b496
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-15 08:13:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@
   provider "ansible" {
     # Configuration options
   }
-resource "ansible_host" "salt-proxy" {
+resource "ansible_host" "node1" {
   count = 1
 
   // 配置机器的 hostname，一般配置为计算资源的 public_ip (或 private_ip)
   inventory_hostname  = "116.62.157.43"
 
   // 配置机器所属分组
-  groups = [node1]
+  groups = ["node1"]
 
   // 传给 ansible 的 vars，可在 playbook 文件中引用
   vars = {
